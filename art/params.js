1765df803275adf196bf344b576663ab8d3626e8
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro-2.local>
2023-06-14 02:17:53 -0400

    Update #3

--- art/params.js
+++ art/params.js
@@ -1,64 +1,26 @@
-let shapeParams = {
-  shapeType: {
-    value: "flower",
-    type: "button-set",
-    arr: ["flower", "polygon", "star"],
-  },
-  numSides: {
-    value: Math.floor(Math.random() * (20 - 3) +),
-    type: "number",
-    icon: "./art/icons/numSides-icon.svg",
-    range: { min: 3, max: 25, step: 1 },
-  },
-  faceSize: {
-    value: Math.random() * (180 - 50) + 50,
-    // type: "number",
-    icon: "./art/icons/faceSize-icon.svg",
-    range: { min: 0, max: 110, step: 1 },
-  },
-  shapeColor: {
-    value: "#F94E56",
-    type: "palette",
-    label: "Shape Color",
-    arr: [
-      "#ffffff",
-      "#F94E56",
-      "#FA9EC1",
-      "#FFD166",
-      "#3AB795",
-      "#6BCADB",
-      "#72ACFD",
-      "#B54098",
-    ],
-  },
-  faceColor: {
-    value: "#C3B091",
-    type: "palette",
-    label: "Face Color",
-    arr: [
-      "#FFE8D9",
-      "#F8C8A6",
-      "#DFCEBE",
-      "#CEC6BB",
-      "#B57575",
-      "#A35D43",
-      "#7D5D4F",
-      "#564141",
-    ],
-  },
-  intensity: {
-    value: 0.01,
-    type: "slider",
-    label: "intensity",
-    range: { min: 0.005, max: 0.05, step: 0.00001 },
-  },
-  outerSize: {
-    value: Math.random() * (160 - 100) + 100,
-    // type: "slider",
-    label: "Outer Color",
-    range: { min: 0, max: 170, step: 1 },
-  },
-};
+let faceSize = Math.random() * (140 - 60) + 60;
+let shapeTypesArr = ["flower", "polygon", "star"];
+let shapeColorArr = [
+  "#ffffff",
+  "#F94E56",
+  "#FA9EC1",
+  "#FFD166",
+  "#3AB795",
+  "#6BCADB",
+  "#72ACFD",
+  "#B54098",
+];
+let skinTonesArr = [
+  "#FFE8D9",
+  "#F8C8A6",
+  "#DFCEBE",
+  "#CEC6BB",
+  "#B57575",
+  "#A35D43",
+  "#7D5D4F",
+  "#564141",
+];
+let eyeHeight = Math.random() * (20 - 8) + 8;
 
 let faceParams = {
   eyeXPos: 0.25,
@@ -69,4 +31,48 @@ let faceParams = {
   eyebrowInnerCornerTilt: -10,
   eyebrowOuterCornerTilt: 0,
   eyeSpacing: 5,
+};
+
+let shapeParams = {
+  shapeType: {
+    value: shapeTypesArr[Math.floor(Math.random() * shapeTypesArr.length)],
+    type: "button-set",
+    arr: shapeTypesArr,
+  },
+  numSides: {
+    value: Math.floor(Math.random() * (20 - 3) + 3),
+    type: "number",
+    icon: "./art/icons/numSides-icon.svg",
+    range: { min: 3, max: 25, step: 1 },
+  },
+  faceSize: {
+    value: faceSize,
+    // type: "number",
+    icon: "./art/icons/faceSize-icon.svg",
+    range: { min: 0, max: 110, step: 1 },
+  },
+  outerSize: {
+    value: Math.random() * (faceSize * 2 - faceSize * 1.1) + faceSize * 1.1,
+    // type: "small-slider",
+    label: "Outer Color",
+    range: { min: 0, max: 170, step: 1 },
+  },
+  shapeColor: {
+    value: shapeColorArr[Math.floor(Math.random() * shapeColorArr.length)],
+    type: "palette",
+    label: "Shape Color",
+    arr: shapeColorArr,
+  },
+  faceColor: {
+    value: skinTonesArr[Math.floor(Math.random() * skinTonesArr.length)],
+    type: "palette",
+    label: "Skin Tone",
+    arr: skinTonesArr,
+  },
+  intensity: {
+    value: Math.random() * (0.02 - 0.005) + 0.005,
+    type: "slider",
+    label: "intensity",
+    range: { min: 0.005, max: 0.05, step: 0.00001 },
+  },
 };
